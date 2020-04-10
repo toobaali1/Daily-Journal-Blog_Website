ed1b213794b96362147b2cb844c965ab40bd4845
Tooba Ali <toobaalined@gmail.com>
2020-04-10 12:23:58 +0500

    Extract post content according to characters and publish

--- app.js
+++ app.js
@@ -7,32 +7,39 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 const titleArray = [];
-const contentArray = [];
+const contentShortArray = [];
+const contentLongArray = [];
+
 
 app.get("/", function(req,res){
     // change keys and values to pass an array
-    res.render("home", {pageTitle: "Home", newPostTitle: titleArray, newPostContent: contentArray}); 
+    res.render("home", {pageTitle: "Home", newPostTitle: titleArray, newPostContent: contentLongArray , xyz:contentShortArray }); 
 
     
 });
 
 app.post("/", function(req,res){ 
-    if(req.body.publishBtn === "publishBtnValue"){ //???????????????????????
        
         var titleOfPost = req.body.postTitle;
         var contentOfPost = req.body.postContent;
 
         //push it in an array!!!  
         titleArray.push(titleOfPost);
-        contentArray.push(contentOfPost);    
-    }
+        contentLongArray.push(contentOfPost);
+        contentShortArray.push(contentOfPost.slice(0,100)); 
+
+       
+    
     res.redirect("/");
 });
+
 
 
 app.get("/about", function(req,res){
     res.render("blog", {pageTitle: "About Us"});
 });
+
+
 
 // app.post("/about", function(req,res){
 
