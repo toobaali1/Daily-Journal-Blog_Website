e71c70b2b699af4b3cb08732ca269b107eba569f
Tooba Ali <toobaalined@gmail.com>
2020-04-11 11:41:54 +0500

    Modify with object array

--- app.js
+++ app.js
@@ -6,57 +6,45 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const titleArray = [];
-const contentShortArray = [];
-const contentLongArray = [];
-
+const blogArray = [];
 
 app.get("/", function(req,res){
     // change keys and values to pass an array
-    res.render("home", {pageTitle: "Home", newPostTitle: titleArray, newPostContent: contentLongArray , xyz:contentShortArray }); 
-
-    
+    res.render("home", {pageTitle: "Home", wholeBlog: blogArray });    
 });
 
-app.post("/", function(req,res){ 
-       
-        var titleOfPost = req.body.postTitle;
-        var contentOfPost = req.body.postContent;
+app.get("/compose", function(req,res){
+    res.render("composePage");
+});
 
-        //push it in an array!!!  
-        titleArray.push(titleOfPost);
-        contentLongArray.push(contentOfPost);
-        contentShortArray.push(contentOfPost.slice(0,100)); 
-
-       
-    
+app.post("/compose", function(req,res){    
+    let blog = {
+        title: req.body.postTitle,
+        content: req.body.postContent
+    }
+    //push it in an array!!!  
+    blogArray.push(blog);
     res.redirect("/");
 });
-
-
 
 app.get("/about", function(req,res){
     res.render("blog", {pageTitle: "About Us"});
 });
 
-
-
-// app.post("/about", function(req,res){
-
-// });
-
 app.get("/contact", function(req,res){
-    // res.send("hello");
     res.render("blog", {pageTitle: "Contact Us"});
 });
 
-// app.post("/contact", function(req,res){
 
-// });
-
-app.get("/compose", function(req,res){
-    res.render("composePage");
+// -------------------------------------------------------------------------------
+app.get("/posts/:postname", function(req,res){
+   console.log(req.params.postname);
+    
 });
+
+// -------------------------------------------------------------------------------
+
+
 
 app.listen(3000, function(){
     console.log("Server running at port 3000");
